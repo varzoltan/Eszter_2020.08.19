0c40497a0356b7bfa3acb79ead8aeb8642870edc
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-08-19 11:10:43 +0200

    11:10

--- Eszter_2020.08.19/Program.cs
+++ Eszter_2020.08.19/Program.cs
@@ -10,9 +10,44 @@ namespace Eszter_2020._08._19
     {
         static void Main(string[] args)
         {
-            //13. A program írja ki, hogy a felhasználó által megadott szám osztóit, 
+            //13. A program írja ki a felhasználó által megadott szám osztóit, 
             //illetve, hogy az adott számnak hány osztója van egyen és önmagán kívül.
+            Console.Write("Kérem adjon meg egy egész számot! ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.Write($"A {szam} osztói: ");
+            int szamlalo = 0;//Összegzés törvénye
+            for (int i = 2; i<= szam/2; i++)
+            {
+                if (szam % i == 0)
+                {
+                    Console.Write($"{i} ");
+                    szamlalo++;
+                }
 
+            }
+            Console.WriteLine();
+            Console.Write($"A {szam}-nak {szamlalo} osztója van.");
+
+            //14. Bekérünk a felhasználótól egy számot, majd a megadott számig kiírja a prímszámokat.
+            Console.Write("Kérem adjon meg egy egész számot! ");
+            int szam1 = int.Parse(Console.ReadLine());
+            Console.Write($"A prímszámok: ");
+            for (int i = 2; i <= szam1; i++)
+            {
+                int szamlalo1 = 0;
+                for (int j = 2;j<=szam1/2;j++)
+                {
+                    if (i % j == 0)
+                    {
+                        szamlalo1++;
+                    }
+                }
+                if (szamlalo1 == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
