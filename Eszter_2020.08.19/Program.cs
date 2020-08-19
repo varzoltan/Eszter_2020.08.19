de3b8a6767ac3b04e344fce41970919fa42839c0
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-08-19 10:02:32 +0200

    Initial commit

---
+++ Eszter_2020.08.19/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eszter_2020._08._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //13. A program írja ki, hogy a felhasználó által megadott szám osztóit, 
+            //illetve, hogy az adott számnak hány osztója van egyen és önmagán kívül.
+        }
+    }
+}
